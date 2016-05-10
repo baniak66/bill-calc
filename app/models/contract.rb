b461991976da8c70b4f2f601ccc401001687ae13
Pawel Licznar <pawellicznar@gmail.com>
2016-05-11 00:48:37 +0200

    Add select field for tax_rate

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,5 @@
 class Contract < ActiveRecord::Base
+  TAX_RATES = { "0%" => 0.0, "20%" => 0.2, "50%" => 0.5 }
   validates :date, :number, :employer_name, :employee_name, :gross_amount,
             :tax_rate, :bill_number, presence: true
 end
