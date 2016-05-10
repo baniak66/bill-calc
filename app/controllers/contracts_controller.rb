7f9f1bab35c0f1f4d85e5080f907fbdc39c1aa2a
Pawel Licznar <pawellicznar@gmail.com>
2016-05-11 00:05:09 +0200

    Add new and create actions with templates

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,4 +2,23 @@ class ContractsController < ApplicationController
 
   def index
   end
+
+  def new
+    @contract = Contract.new
+  end
+
+  def create
+    @contract = Contract.create(contract_params)
+    if @contract.save
+      redirect_to contracts_path
+    else
+      render "new"
+    end
+  end
+
+  private
+    def contract_params
+      params.require(:contract).permit(:date, :number, :employer_name, :employee_name, :gross_amount, :tax_rate, :bill_number)
+    end
+
 end
