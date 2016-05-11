2b173ef919833272400b8727b285abb18f5923c9
Pawel Licznar <pawellicznar@gmail.com>
2016-05-11 23:35:11 +0200

    Add generate_bill action and route

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -21,6 +21,15 @@ class ContractsController < ApplicationController
     end
   end
 
+  def generate_bill
+    @contract = Contract.find(params[:id])
+    respond_to do |format|
+      format.pdf do
+        render pdf: "Bill #{@contract.bill_number}"
+      end
+    end
+  end
+
   private
     def contract_params
       params.require(:contract).permit(:date, :number, :employer_name, :employee_name, :gross_amount, :cost_rate, :bill_number)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'contracts#index'
   resources :contracts
+  get 'generate_bill', to: 'contracts#generate_bill'
 
 end
