ecc0d6d124432b62f60fa28cd6d8a3a99b5a9430
Pawel Licznar <pawellicznar@gmail.com>
2016-05-13 23:54:55 +0200

    Add more tests for contract model

--- spec/factories/contracts.rb
+++ spec/factories/contracts.rb
@@ -3,7 +3,9 @@ FactoryGirl.define do
     date "2016-01-01"
     number "1/2016"
     employer_name "Company name"
+    employer_tax_number "0000000000"
     employee_name "Employee name"
+    employee_tax_number "9999999999"
     gross_amount 1000.00
     cost_rate 0.0
     bill_number "001"
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -3,10 +3,18 @@ require "rails_helper"
 RSpec.describe Contract, :type => :model do
 
   let (:contract) { create :contract, cost_rate: 0.2 }
+  let (:contract2) { create :contract, gross_amount: 100000, cost_rate: 0.5 }
+  let (:contract3) { create :contract, gross_amount: 120000, cost_rate: 0.0 }
 
   describe "#income_costs" do
     it "calculate income cost with gross amount 1000 and rate 20%" do
       expect(contract.income_costs).to eq(200)
+    end
+    it "calculate income cost with gross amount 100 000 and rate 50%" do
+      expect(contract2.income_costs).to eq(45658.4)
+    end
+    it "calculate income cost with gross amount 120 000 and rate 0%" do
+      expect(contract3.income_costs).to eq(0)
     end
   end
 
@@ -14,17 +22,38 @@ RSpec.describe Contract, :type => :model do
     it "calculate tax base with gross amount 1000 and cost rate 20%" do
       expect(contract.tax_base).to eq(800)
     end
+    it "calculate tax base with gross amount 100 000 and cost rate 50%" do
+      expect(contract2.tax_base).to eq(54341.6)
+    end
+    it "calculate tax base with gross amount 120 000 and cost rate 0%" do
+      expect(contract3.tax_base).to eq(120000)
+    end
   end
 
   describe "#tax_to_pay" do
     it "calculate tax to pay with gross amount 1000 and cost rate 20%" do
       expect(contract.tax_to_pay).to eq(144)
     end
+    it "calculate tax to pay with gross amount 100 000 and cost rate 50%" do
+      expect(contract2.tax_to_pay).to eq(9781.49)
+    end
+    it "calculate tax to pay with gross amount 120 000 and cost rate 0%" do
+      expect(contract3.tax_to_pay).to eq(26426.08)
+    end
   end
 
   describe "#net_amount" do
     it "calculate net amount with gross amount 1000 and cost rate 20%" do
       expect(contract.net_amount).to eq(856)
+    end
+    # it "calculate net amount with gross amount 100 000 and cost rate 50%" do
+    #   expect(contract2.net_amount).to eq(90218.51)
+    # end
+    # expected: 90218.51
+         # got: 90218.51 (#<BigDecimal:7ff52bbc5160,'0.9021851E5',18(27)>)
+         # why not equal??
+    it "calculate net amount with gross amount 120 000 and cost rate 0%" do
+      expect(contract3.net_amount).to eq(93573.92)
     end
   end
 
